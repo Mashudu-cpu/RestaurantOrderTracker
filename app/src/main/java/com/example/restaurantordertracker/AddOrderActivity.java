a9500818be96001e60b910c56e6da2cdba8398f5
MashCPU <mashudunndwambi1@gmail.com>
2026-05-14 19:46:08 +0200

    I added some functions for our UI

---
+++ app/src/main/java/com/example/restaurantordertracker/AddOrderActivity.java
@@ -0,0 +1,13 @@
+package com.example.restaurantordertracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddOrderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstancestate){
+        super.onCreate(savedInstancestate);
+        setContentView(R.layout.add_order);
+    }
+}
